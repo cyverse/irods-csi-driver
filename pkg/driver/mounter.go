0e9fd67deb83b95ca2fc64baa13111afc15adba5
Illyoung Choi <iychoi@email.arizona.edu>
2020-06-26 11:51:10 -0700

    Implement mounter

--- pkg/driver/mounter.go
+++ pkg/driver/mounter.go
@@ -35,7 +35,7 @@ func newNodeMounter() Mounter {
 	}
 }
 
-func (m *NodeMounter) MakeDir(pathname string) error {
+func (mounter *NodeMounter) MakeDir(pathname string) error {
 	err := os.MkdirAll(pathname, os.FileMode(0755))
 	if err != nil {
 		if !os.IsExist(err) {
@@ -45,6 +45,35 @@ func (m *NodeMounter) MakeDir(pathname string) error {
 	return nil
 }
 
-func (m *NodeMounter) GetDeviceName(mountPath string) (string, int, error) {
-	return mount.GetDeviceNameFromMount(m, mountPath)
+func (mounter *NodeMounter) GetDeviceName(mountPath string) (string, int, error) {
+	return mount.GetDeviceNameFromMount(mounter, mountPath)
+}
+
+// Mount mounts source to target as fstype with given options. 'source' and 'fstype' must
+// be an empty string in case it's not required, e.g. for remount, or for auto filesystem
+// type, where kernel handles fstype for you. The mount 'options' is a list of options,
+// currently come from mount(8), e.g. "ro", "remount", "bind", etc. If no more option is
+// required, call Mount with an empty string list or nil.
+func (mounter *NodeMounter) Mount(source string, target string, fstype string, options []string) error {
+	return mounter.MountSensitive(source, target, fstype, options, nil)
+}
+
+// MountSensitive is the same as Mount() but this method allows
+// sensitiveOptions to be passed in a separate parameter from the normal
+// mount options and ensures the sensitiveOptions are never logged. This
+// method should be used by callers that pass sensitive material (like
+// passwords) as mount options.
+func (mounter *NodeMounter) MountSensitive(source string, target string, fstype string, options []string, sensitiveOptions []string) error {
+	// Path to mounter binary if containerized mounter is needed. Otherwise, it is set to empty.
+	// All Linux distros are expected to be shipped with a mount utility that a support bind mounts.
+	bind, bindOpts, bindRemountOpts, bindRemountOptsSensitive := mount.MakeBindOptsSensitive(options, sensitiveOptions)
+	if bind {
+		err := mounter.doMount("", defaultMountCommand, source, target, fstype, bindOpts, bindRemountOptsSensitive)
+		if err != nil {
+			return err
+		}
+		return mounter.doMount("", defaultMountCommand, source, target, fstype, bindRemountOpts, bindRemountOptsSensitive)
+	}
+
+	return mounter.doMount(mounter.mounterPath, defaultMountCommand, source, target, fstype, options, sensitiveOptions)
 }
---
+++ pkg/driver/node.go
@@ -0,0 +1,229 @@
+    /*
+Copyright 2019 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package driver
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/klog"
+)
+
+var (
+	nodeCaps = []csi.NodeServiceCapability_RPC_Type{}
+	volumeCapAccessModes = []csi.VolumeCapability_AccessMode_Mode{
+		csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+		csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER,
+	}
+)
+
+// this is for dynamic volume provisioning
+func (driver *Driver) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// this is for dynamic volume provisioning
+func (driver *Driver) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (driver *Driver) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	klog.V(4).Infof("NodePublishVolume: called with args %+v", req)
+
+	target := req.GetTargetPath()
+	if len(target) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path not provided")
+	}
+
+	volCap := req.GetVolumeCapability()
+	if volCap == nil {
+		return nil, status.Error(codes.InvalidArgument, "Volume capability not provided")
+	}
+
+	if !driver.isValidVolumeCapabilities([]*csi.VolumeCapability{volCap}) {
+		return nil, status.Error(codes.InvalidArgument, "Volume capability not supported")
+	}
+
+	// TODO when CreateVolume is implemented, it must use the same key names
+	subpath := "/"
+	volContext := req.GetVolumeContext()
+	for k, v := range volContext {
+		switch strings.ToLower(k) {
+		//Deprecated
+		case "path":
+			klog.Warning("Use of path under volumeAttributes is depracated. This field will be removed in future release")
+			if !filepath.IsAbs(v) {
+				return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Volume context property %q must be an absolute path", k))
+			}
+			subpath = filepath.Join(subpath, v)
+		default:
+			return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Volume context property %s not supported", k))
+		}
+	}
+
+	volumeId := req.GetVolumeId()
+
+	var source string
+	tokens := strings.Split(volumeId, ":")
+	if len(tokens) == 1 {
+		// fs-xxxxxx
+		source = fmt.Sprintf("%s:%s", volumeId, subpath)
+	} else if len(tokens) == 2 {
+		// fs-xxxxxx:/a/b/c
+		cleanPath := path.Clean(tokens[1])
+		source = fmt.Sprintf("%s:%s", tokens[0], cleanPath)
+	}
+
+	mountOptions := []string{}
+	if req.GetReadonly() {
+		mountOptions = append(mountOptions, "ro")
+	}
+
+	if m := volCap.GetMount(); m != nil {
+		hasOption := func(options []string, opt string) bool {
+			for _, o := range options {
+				if o == opt {
+					return true
+				}
+			}
+			return false
+		}
+		for _, f := range m.MountFlags {
+			if !hasOption(mountOptions, f) {
+				mountOptions = append(mountOptions, f)
+			}
+		}
+	}
+
+	klog.V(5).Infof("NodePublishVolume: creating dir %s", target)
+	if err := driver.mounter.MakeDir(target); err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not create dir %q: %v", target, err)
+	}
+
+	var fsType string
+	switch(driver.config.DriverType) {
+	    case fuseType:
+			fsType = "irodsfs"
+	    case nfsType:
+			fsType = "nfs"
+	    case webdavType:
+			fsType = "webdav"
+	    default:
+	        return nil, status.Errorf(codes.Internal, "unknown driver type - %v", driver.config.DriverType)
+    }
+
+	klog.V(5).Infof("NodePublishVolume: mounting %s (%s) at %s with options %v", source, fsType, target, mountOptions)
+
+	// TODO: need to consider calling Mountsensitive
+	if err := driver.mounter.Mount(source, target, fsType, mountOptions); err != nil {
+		os.Remove(target)
+		return nil, status.Errorf(codes.Internal, "Could not mount %q (%q) at %q: %v", source, fsType, target, err)
+	}
+	klog.V(5).Infof("NodePublishVolume: %s was mounted", target)
+
+	return &csi.NodePublishVolumeResponse{}, nil
+}
+
+func (driver *Driver) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
+	klog.V(4).Infof("NodeUnpublishVolume: called with args %+v", req)
+
+	target := req.GetTargetPath()
+	if len(target) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path not provided")
+	}
+
+	// Check if target directory is a mount point. GetDeviceNameFromMount
+	// given a mnt point, finds the device from /proc/mounts
+	// returns the device name, reference count, and error code
+	_, refCount, err := driver.mounter.GetDeviceName(target)
+	if err != nil {
+		msg := fmt.Sprintf("failed to check if volume is mounted: %v", err)
+		return nil, status.Error(codes.Internal, msg)
+	}
+
+	// From the spec: If the volume corresponding to the volume_id
+	// is not staged to the staging_target_path, the Plugin MUST
+	// reply 0 OK.
+	if refCount == 0 {
+		klog.V(5).Infof("NodeUnpublishVolume: %s target not mounted", target)
+		return &csi.NodeUnpublishVolumeResponse{}, nil
+	}
+
+	klog.V(5).Infof("NodeUnpublishVolume: unmounting %s", target)
+	err = driver.mounter.Unmount(target)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not unmount %q: %v", target, err)
+	}
+	klog.V(5).Infof("NodeUnpublishVolume: %s unmounted", target)
+
+	return &csi.NodeUnpublishVolumeResponse{}, nil
+}
+
+func (driver *Driver) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVolumeStatsRequest) (*csi.NodeGetVolumeStatsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (driver *Driver) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandVolumeRequest) (*csi.NodeExpandVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (driver *Driver) NodeGetCapabilities(ctx context.Context, req *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
+	klog.V(4).Infof("NodeGetCapabilities: called with args %+v", req)
+	var caps []*csi.NodeServiceCapability
+	for _, cap := range nodeCaps {
+		c := &csi.NodeServiceCapability{
+			Type: &csi.NodeServiceCapability_Rpc{
+				Rpc: &csi.NodeServiceCapability_RPC{
+					Type: cap,
+				},
+			},
+		}
+		caps = append(caps, c)
+	}
+	return &csi.NodeGetCapabilitiesResponse{Capabilities: caps}, nil
+}
+
+func (driver *Driver) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoRequest) (*csi.NodeGetInfoResponse, error) {
+	klog.V(4).Infof("NodeGetInfo: called with args %+v", req)
+
+	return &csi.NodeGetInfoResponse{
+		NodeId: driver.nodeID,
+	}, nil
+}
+
+func (driver *Driver) isValidVolumeCapabilities(volCaps []*csi.VolumeCapability) bool {
+	hasSupport := func(cap *csi.VolumeCapability) bool {
+		for _, m := range volumeCapAccessModes {
+			if m == cap.AccessMode.GetMode() {
+				return true
+			}
+		}
+		return false
+	}
+
+	foundAll := true
+	for _, c := range volCaps {
+		if !hasSupport(c) {
+			foundAll = false
+		}
+	}
+	return foundAll
+}
