871d62c19c4f32eca1e9aa5661a7754773ffa432
Illyoung Choi <iychoi@email.arizona.edu>
2020-11-12 14:27:07 -0700

    Update license

--- pkg/driver/mounter.go
+++ pkg/driver/mounter.go
@@ -1,18 +1,29 @@
 /*
-Copyright 2019 The Kubernetes Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+Following functions or objects are from the code under APL2 License.
+- Mounter
+- NodeMounter
+- newNodeMounter
+- GetDeviceName
+- Mount
+- MountSensitive
+- doMount
+- MakeMountArgsSensitive
+- sanitizedOptionsForLogging
+- Unmount
+- List
+- ListProcMounts
+- parseProcMounts
+- IsLikelyNotMountPoint
+- GetMountRefs
+- SearchMountPoints
+- PathExists
+- MakeDir
+Original code:
+- https://github.com/kubernetes-sigs/aws-efs-csi-driver/blob/master/pkg/driver/mounter.go
+- https://github.com/kubernetes/utils/blob/master/mount/mount.go
 
-/*
-Copyright 2020 CyVerse
+
+Copyright 2019 The Kubernetes Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
