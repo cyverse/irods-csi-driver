899247979520c16a083ffaf5ad58b49b10c3eb53
Illyoung Choi <iychoi@email.arizona.edu>
2021-10-05 12:20:02 -0700

    Fix argument passing to CSI Driver

--- deploy/image/start.sh
+++ deploy/image/start.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+echo "Starting iRODS FUSE Lite Pool Service"
 # execute irodsfs-pool
 /usr/bin/irodsfs-pool
 
+echo "Starting iRODS CSI Driver"
 # execute irods-csi-driver
-/bin/irods-csi-driver %@
+/bin/irods-csi-driver "$@"
