a8aa80d05e6813b93b0c6f29c3c3819356ae6799
Illyoung Choi <iychoi@email.arizona.edu>
2023-07-06 13:55:08 -0700

    Run the driver on every k8s node

--- nagios/check_irodscsidriver_restart.py
+++ nagios/check_irodscsidriver_restart.py
@@ -8,7 +8,8 @@
 
 import os, sys
 
-kubecommand = "kubectl get pods -n irods-csi-driver -o wide --no-headers"
+hostname = os.getenv("HOSTNAME")
+kubecommand = "kubectl get pods -n irods-csi-driver -o wide --no-headers --field-selector spec.nodeName=%s" % hostname
 kubeconf = ""
 if len(sys.argv) >= 2:
     kubeconf = sys.argv[1]
@@ -30,25 +31,25 @@ for line in pipe:
     podname = fields[0].strip()
     status = fields[2].strip()
     restarts = int(fields[3].strip())
-    node = "%s(%s)" % (fields[6].strip(), fields[5].strip())
+    msg = "%s(%s)" % (podname, fields[6].strip())
 
     if fields[4].startswith("("):
         for i in range(4, 7):
             if fields[i].strip().endswith(")"):
-                node = "%s(%s)" % (fields[i+3].strip(), fields[i+2].strip())
+                msg = "%s(%s)" % (podname, fields[i+3].strip())
 
     if not podname.startswith("irods-csi-driver-node"):
         continue
 
     if status.lower() not in ["running"]:
-        stopped_pods.append(node)
+        stopped_pods.append(msg)
         continue
 
     if restarts > 0:
-        restarted_pods.append(node)
+        restarted_pods.append(msg)
 
         if restarts > 10:
-            restarted_toomany_pods.append(node)
+            restarted_toomany_pods.append(msg)
         continue
     
 
@@ -57,15 +58,15 @@ if len(restarted_pods) == 0 and len(stopped_pods) == 0:
     sys.exit(0)
 elif len(stopped_pods) > 0:
     print_pods = ', '.join(stopped_pods)
-    print("CRITICAL - iRODS CSI Drivers are not running on [%s] nodes." % print_pods)
+    print("CRITICAL - iRODS CSI Drivers are not running. Failed pods are [%s]." % print_pods)
     sys.exit(2)
 elif len(restarted_toomany_pods) > 0:
     print_pods = ', '.join(restarted_toomany_pods)
-    print("CRITICAL - iRODS CSI Drivers are restarted more than 10 times on [%s] nodes. Check irodsfs mounts." % print_pods)
+    print("CRITICAL - iRODS CSI Drivers are restarted more than 10 times. Restarted pods are [%s]. Check irodsfs mounts." % print_pods)
     sys.exit(2)
 elif len(restarted_pods) > 0:
     print_pods = ', '.join(restarted_pods)
-    print("WARNING - iRODS CSI Drivers are restarted on [%s] nodes. Check irodsfs mounts." % print_pods)
+    print("WARNING - iRODS CSI Drivers are restarted. Restarted pods are [%s]. Check irodsfs mounts." % print_pods)
     sys.exit(1)
 else:
     print("UKNOWN - iRODS CSI Driver status unknown")
